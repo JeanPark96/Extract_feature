d19083e0db21ff271f675bf27adfe4b06c52cb6f
JeanPark96 <hlpark@seas.upenn.edu>
2023-06-26 10:24:39 -0400

    update extract code and csv file

--- extract/extract.py
+++ extract/extract.py
@@ -4,12 +4,24 @@ import numpy as np
 import torch.nn.functional as F
 from tqdm import tqdm
 import argparse
-from extract.video_loader import VideoLoader
+import pandas as pd
+from video_loader import VideoLoader
 from torch.utils.data import DataLoader
-from extract.preprocessing import Preprocessing
-from extract.random_sequence_shuffler import RandomSequenceSampler
-from args import MODEL_DIR
+from preprocessing import Preprocessing
+from random_sequence_shuffler import RandomSequenceSampler
+import os
+# from args import MODEL_DIR
 import clip
+
+
+scenic_zeroshot_eval_folder_path = "" #add
+video_name = "" #add
+feature_name = "" #add
+extract_feature_csv_path = "" #add
+video_path = os.path.join("video", video_name)
+feature_path = os.path.join("feature", feature_name)
+df = pd.DataFrame([[os.path.join(scenic_zeroshot_eval_folder_path, video_path), os.path.join(scenic_zeroshot_eval_folder_path, feature_path)]], columns = ["video_path", "feature_path"])
+df.to_csv(extract_feature_csv_path, index=None)
 
 parser = argparse.ArgumentParser(description="Easy video feature extractor")
 
@@ -51,6 +63,7 @@ dataset = VideoLoader(
     centercrop=True,
 )
 n_dataset = len(dataset)
+print(n_dataset)
 sampler = RandomSequenceSampler(n_dataset, 10)
 loader = DataLoader(
     dataset,
@@ -59,12 +72,25 @@ loader = DataLoader(
     num_workers=args.num_decoding_thread,
     sampler=sampler if n_dataset > 10 else None,
 )
+print(len(loader))
 preprocess = Preprocessing()
-model, _ = clip.load("ViT-L/14", download_root=MODEL_DIR)
+model, _ = clip.load("ViT-L/14") #, download_root=MODEL_DIR
+
+#load fine-tuned clip
+# checkpoint = th.load("/home/hlpark/shared/REDUCE/clip/finetune/model_checkpoint/best_model.pt")
+
+# Use these 3 lines if you use default model setting(not training setting) of the clip. For example, if you set context_length to 100 since your string is very long during training, then assign 100 to checkpoint['model_state_dict']["context_length"] 
+# checkpoint['model_state_dict']["input_resolution"] = model.input_resolution #default is 224
+# checkpoint['model_state_dict']["context_length"] = model.context_length # default is 77
+# checkpoint['model_state_dict']["vocab_size"] = model.vocab_size 
+
+# model.load_state_dict(checkpoint['model_state_dict'])
+
 model.eval()
 model = model.cuda()
 
 with th.no_grad():
+    
     for k, data in enumerate(loader):
         input_file = data["input"][0]
         output_file = data["output"][0]
@@ -80,10 +106,12 @@ with th.no_grad():
                 n_chunk = len(video)
                 features = th.cuda.FloatTensor(n_chunk, args.feature_dim).fill_(0)
                 n_iter = int(math.ceil(n_chunk / float(args.batch_size)))
+                print(n_chunk, n_iter)
                 for i in tqdm(range(n_iter)):
                     min_ind = i * args.batch_size
                     max_ind = (i + 1) * args.batch_size
                     video_batch = video[min_ind:max_ind].cuda()
+                    #print(video_batch)
                     batch_features = model.encode_image(video_batch)
                     if args.l2_normalize:
                         batch_features = F.normalize(batch_features, dim=1)
